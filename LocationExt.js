7fa079830fc26855665365711c26ff94d7121e80
Yufeng Lee <lyfxj87@gmail.com>
2016-01-30 14:44:22 +0800

    浏览器中对 Location 对象的扩展

---
+++ LocationExt.js
@@ -0,0 +1,39 @@
+(function(location){
+    var search = location.search.substr(1);
+    var query = new Array();
+    if (search.length != 0) {
+        var tmp = search.split('&');
+        var length = tmp.length;
+        for(var i=0; i < length; i++){
+            var field = tmp[i].split('=');
+            if (field.length != 2) {
+                continue;
+            }
+            query[field[0]] = field[1];
+        }
+    }
+    location.query = query;    
+    location.setQuery = function(k, v){
+        this.query[k] = v;
+    };
+    location.unsetQuery = function(k){
+        delete this.query[k];
+    };
+    location.queryString = function(){
+        var tmp = new Array();
+        for (var k in this.query) {
+            var v = this.query;
+            if (v === '' || v === undefined || v === null) {
+                continue;
+            }
+            tmp.push( k + '=' + this.query[k] );
+        }
+        if (tmp.length==0) {
+            return '';
+        } else {
+            return tmp.join('&');
+        }
+    };
+
+    return location;
+})(window.location);
